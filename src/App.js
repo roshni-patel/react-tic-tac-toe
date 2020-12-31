216739c15ab3a56254e21ee69c085a6aa9f7831f
marks214 <aimee.oz.x@gmail.com>
2020-12-30 16:57:31 -0800

    set winner is not working

--- src/App.js
+++ src/App.js
@@ -29,9 +29,11 @@ const App = () => {
 
   const [squares, setSquares] = useState(generateSquares());
   const [turnNumber, setTurnNumber] = useState(0);
+  const [winner, setWinner] = useState(null);
   const [player, setPlayer] = useState(PLAYER_1);
 
   const onClickCallback = (id) => {
+    if (winner === null) {
     const selected = squares;
     for (let row = 0; row < 3; row += 1) {
       for (let col = 0; col < 3; col += 1) {
@@ -43,32 +45,48 @@ const App = () => {
       }
     }
     setSquares(selected);
+    setWinner(checkForWinner());
 
     // if turnNumber == 9 call on checkForWinner - this means all the squares are now full
   
   }
+}
 
   const checkForWinner = () => {
-    // these are all the ways you can win at tic-tac-toe (https://forum.freecodecamp.org/t/logic-for-tic-tac-toe-win/171590)
     const winCombos = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9],
-      [1, 5, 9],
-      [3, 5, 7],
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
       [1, 4, 7],
       [2, 5, 8],
-      [3, 6, 9],
+      [0, 4, 8],
+      [2, 4, 6]
     ]
-// maybe not nested? maybe just check that all three values of the row are either all X or all O?
-    for (let row = 0; row < winCombos.length; row++) {
-      for (let col = 0; col < 3; col++) {
-        //psuedocode 
-        // if (winCombos[row][col] == "X" for all three values in the row) {then X won}
-        // else if (winCombos[row][col] == "O" for all three columns in the row) {then O won} 
-        // else it's a draw, no one won
+    const [firstRow, secondRow, thirdRow] = squares;
+    const board = [ ...firstRow, ...secondRow, ...thirdRow];
+    const xOPositions = [];
+    
+    for (let i = 0; i < winCombos.length; i++) {
+      
+      const position1 = board[winCombos[i][0]]
+      const position2 = board[winCombos[i][1]]
+      const position3 = board[winCombos[i][2]]
+      xOPositions.push([position1, position2, position3])
+    }
+
+    for (let i = 0; i < xOPositions; i++) {
+      if (xOPositions[i][0] === 'X' && xOPositions[i][1] === 'X' && xOPositions[i][2] === 'X') {
+        setWinner(PLAYER_1)
+        return PLAYER_1
+      }
+      else if (xOPositions[i][0] === 'O' && xOPositions[i][1] === 'O' && xOPositions[i][2] === 'O') {
+        setWinner(PLAYER_2)
+        return PLAYER_2
       }
     }
+
+    setWinner(null)
 
   }
 
@@ -81,7 +99,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... -- Fill in for wave 3 </h2>
+        <h2>The winner is {winner}</h2>
         <button>Reset Game</button>
       </header>
       <main>
